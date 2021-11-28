8e024f1adff01351f77537956ae44704f66e73c8
Filip Mielczarek <mielczarek.filip.1998@gmail.com>
2021-11-28 20:05:57 +0100

    Implement basic functionality

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const LiveReloadPlugin = require("webpack-livereload-plugin");
+
+module.exports = {
+  mode: "production",
+  target: "browserslist",
+
+  output: {
+    assetModuleFilename: "images/[hash][ext][query]",
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(png|jp?g|gif|svg|webp)$/i,
+        type: "asset",
+      },
+      {
+        test: /\.(s[ac]|c)ss$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: { publicPath: "" },
+          },
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+
+  plugins: [new LiveReloadPlugin(), new MiniCssExtractPlugin()],
+
+  devtool: "source-map",
+};
