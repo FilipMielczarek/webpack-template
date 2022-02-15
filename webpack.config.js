592421e0123425dd07dd001882b1e3ee3ce8a4be
Filip Mielczarek <mielczarek.filip.1998@gmail.com>
2022-02-15 13:45:55 +0100

    Change breakpoints and fix live-reload-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,13 @@
 const path = require("path");
 const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const {CleanWebpackPlugin} = require("clean-webpack-plugin");
+const LiveReloadPlugin = require("webpack-livereload-plugin");
+
 
 let mode = "development";
 let target = "web";
-const plugins = [
-  new CleanWebpackPlugin(),
-  new MiniCssExtractPlugin(),
-];
+const plugins = [new LiveReloadPlugin(), new CleanWebpackPlugin(), new MiniCssExtractPlugin(),];
 
 if (process.env.NODE_ENV === "production") {
   mode = "production";
@@ -31,68 +30,52 @@ module.exports = {
 
   output: {
     // output path is required for `clean-webpack-plugin`
-    path: path.resolve(__dirname, "dist"),
-    // this places all images processed in an image folder
+    path: path.resolve(__dirname, "dist"), // this places all images processed in an image folder
     assetModuleFilename: "images/[hash][ext][query]",
   },
 
   module: {
-    rules: [
-      {
-        test: /\.(s[ac]|c)ss$/i,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            // This is required for asset imports in CSS, such as url()
-            options: { publicPath: "" },
-          },
-          "css-loader",
-          "postcss-loader",
-          // according to the docs, sass-loader should be at the bottom, which
-          // loads it first to avoid prefixes in your sourcemaps and other issues.
-          "sass-loader",
-        ],
-      },
-      {
-        test: /\.(png|jpe?g|gif|svg)$/i,
-        /**
-         * The `type` setting replaces the need for "url-loader"
-         * and "file-loader" in Webpack 5.
-         *
-         * setting `type` to "asset" will automatically pick between
-         * outputing images to a file, or inlining them in the bundle as base64
-         * with a default max inline size of 8kb
-         */
-        type: "asset",
+    rules: [{
+      test: /\.(s[ac]|c)ss$/i, use: [{
+        loader: MiniCssExtractPlugin.loader, // This is required for asset imports in CSS, such as url()
+        options: {publicPath: ""},
+      }, "css-loader", "postcss-loader", // according to the docs, sass-loader should be at the bottom, which
+        // loads it first to avoid prefixes in your sourcemaps and other issues.
+        "sass-loader",],
+    }, {
+      test: /\.(png|jpe?g|gif|svg)$/i, /**
+       * The `type` setting replaces the need for "url-loader"
+       * and "file-loader" in Webpack 5.
+       *
+       * setting `type` to "asset" will automatically pick between
+       * outputing images to a file, or inlining them in the bundle as base64
+       * with a default max inline size of 8kb
+       */
+      type: "asset",
 
-        /**
-         * If you want to inline larger images, you can set
-         * a custom `maxSize` for inline like so:
-         */
-        // parser: {
-        //   dataUrlCondition: {
-        //     maxSize: 30 * 1024,
-        //   },
-        // },
-      },
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        use: {
-          // without additional settings, this will reference .babelrc
-          loader: "babel-loader",
-          options: {
-            /**
-             * From the docs: When set, the given directory will be used
-             * to cache the results of the loader. Future webpack builds
-             * will attempt to read from the cache to avoid needing to run
-             * the potentially expensive Babel recompilation process on each run.
-             */
-            cacheDirectory: true,
-          },
+      /**
+       * If you want to inline larger images, you can set
+       * a custom `maxSize` for inline like so:
+       */
+      // parser: {
+      //   dataUrlCondition: {
+      //     maxSize: 30 * 1024,
+      //   },
+      // },
+    }, {
+      test: /\.jsx?$/, exclude: /node_modules/, use: {
+        // without additional settings, this will reference .babelrc
+        loader: "babel-loader", options: {
+          /**
+           * From the docs: When set, the given directory will be used
+           * to cache the results of the loader. Future webpack builds
+           * will attempt to read from the cache to avoid needing to run
+           * the potentially expensive Babel recompilation process on each run.
+           */
+          cacheDirectory: true,
         },
       },
-    ],
+    },],
   },
 
   plugins: plugins,
@@ -107,7 +90,6 @@ module.exports = {
 
   // required if using webpack-dev-server
   devServer: {
-    contentBase: "./dist",
-    hot: true,
+    contentBase: "./dist", hot: true,
   },
 };
